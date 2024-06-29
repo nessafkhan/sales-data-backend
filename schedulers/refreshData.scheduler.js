8823cf34b89bfe8fef3714bc19832c0e8dc6eba9
nessafkhan <nessafkhan.kn@gmail.com>
2024-06-29 17:02:55 +0530

    cron setup for data refresh

--- schedulers/refreshData.scheduler.js
+++ schedulers/refreshData.scheduler.js
@@ -2,10 +2,10 @@ const cron = require('node-cron');
 const refreshData = require('../helper/refreshData.helper');
 
 const scheduleDataRefresh = () => {
-    cron.schedule('0 0 * * *', async () => {
-        console.info('Starting scheduled data refresh...');
-        await refreshData();
-    });
+  cron.schedule('*/1 * * * *', async () => {
+    console.info('Starting scheduled data refresh...');
+    await refreshData();
+  });
 };
 
 module.exports = scheduleDataRefresh;
